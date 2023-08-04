848758ff95495c6a63801a271fe84fb76df22bae
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-08-04 16:10:12 -0300

    applying dip

--- src/interfaceSegregationPrinciple/Main.kt
+++ src/interfaceSegregationPrinciple/Main.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 fun main() {
     val documentRepository = DocumentRepository()
-    val textDocument = TextDocumentTxt("src/interfaceSegregationPrinciple/resources/solid.txt")
+    val textDocument:TextDocument = TextDocumentTxt("src/interfaceSegregationPrinciple/resources/solid.txt")
     textDocument.open()
     textDocument.write("Single Responsiblity Principle\nOpen-Closed Principle\nLiskov Substitution Principle\nInterface Segregation Principle\nDependency Inversion Principle")
     textDocument.read()
@@ -12,7 +12,7 @@ fun main() {
 
     println()
     val imageFile = File("src/interfaceSegregationPrinciple/resources/certifcado_culturadefeedback.PNG")
-    val imageDocument = ImageDocumenImpl(imageFile)
+    val imageDocument:ImageDocument = ImageDocumenImpl(imageFile)
     println("Altura da imagem: ${imageDocument.getImageHeight()}")
     println("Largura da imagem: ${imageDocument.getImageWidth()}")
 
